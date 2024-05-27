23150f79fc94b0592f4e8b1fbcf8cfe682a5fe45
dhruv <120891368+Dhruv556913@users.noreply.github.com>
2024-05-27 11:11:04 +0530

    Update image update.php

--- image update.php
+++ image update.php
@@ -1,3 +1,10 @@
+<img src="img/<?php echo $img; ?>" style="height:100px;width:120px;display:inline;margin-right:20px;float:left;" /><br /><br /><input type="file" name="img" id="img" style="float:left;" />
+<input type="hidden" name="img" id="img"  
+value="<?php if(isset($_SERVER['PHP_SELF']))
+		{echo $img;} ?>"/>
+
+
+
 if (isset($_FILES['image'])) {
         $cat_id = $_POST['cat_id'];
         $pro_name = $_POST['pro_name'];
